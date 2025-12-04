9f44e9d67aaf6328f1132a223a61642cb4a0a12d
Willamy Silva <willamycurso@gmail.com>
2025-12-04 07:58:21 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+import pyodbc
+
+app = FastAPI()
+
+def get_connection():
+    return pyodbc.connect(
+        "DRIVER={ODBC Driver 17 for SQL Server};"
+        "SERVER=pbdb3073.database.windows.net;"
+        "DATABASE=PBDB3073;"
+        "UID=admrs;"
+        "PWD=Gf3$Rn8!Qb12^KsW0tZ;"
+    )
+
+@app.get("/")
+def root():
+    return {"status": "API ONLINE"}
+
+@app.get("/visitas")
+def get_visitas():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    query = """
+    SELECT TOP 10 *
+    FROM TAB_REGISTRO_VISITA_SUPERVISAO
+    """
+
+    cursor.execute(query)
+    columns = [col[0] for col in cursor.description]
+    rows = cursor.fetchall()
+
+    result = [dict(zip(columns, row)) for row in rows]
+
+    cursor.close()
+    conn.close()
+
+    return result
